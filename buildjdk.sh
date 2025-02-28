d8a7a9b1bf679b8fcb4395c62ca5109b1e11add5
AribYadi <aribyadi@gmail.com>
2025-03-01 05:32:28 +0700

    a

--- buildjdk.sh
+++ buildjdk.sh
@@ -94,7 +94,6 @@ bash ./configure \
     --with-jdk-variant=normal \
     --with-jvm-variants="${JVM_VARIANTS/AND/,}" \
     --with-cups-include=$CUPS_DIR \
-    --with-devkit=$TOOLCHAIN \
     --with-debug-level=$JDK_DEBUG_LEVEL \
     --with-fontconfig-include=$ANDROID_INCLUDE \
     --with-freetype-lib=$FREETYPE_DIR/lib \
